5e7e99f9aed16f641140f65c90d44121aa6db39d
Piotr Kowalski <piecioshka@gmail.com>
2014-12-13 10:12:00 +0100

    Create script to build.

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,11 @@
     var shell = require('gulp-shell');
     var markdownpdf = require('gulp-markdown-pdf');
     var rename = require("gulp-rename");
+    var NwBuilder = require('node-webkit-builder');
 
     // node.js modules
     var path = require('path');
+    var del = require('del');
     var cjson = require('cjson');
 
     // configuration for JSHint
@@ -28,7 +30,7 @@
     gulp.task('lint', 'Validate each *.js file with JSHint.', function () {
         return gulp.src(paths.scripts)
             .pipe(jshint(jshintrc))
-            .pipe(jshint.reporter(stylish, { verbose: true }));
+            .pipe(jshint.reporter(stylish, {verbose: true}));
     });
 
     gulp.task('count', 'Count LOC of each *.js file in `app/scripts/core`.', shell.task([
@@ -38,8 +40,41 @@
     gulp.task('build-help', 'Building help.pdf from README.md.', function () {
         return gulp.src('README.md')
             .pipe(markdownpdf())
-            .pipe(rename("docs/help.pdf"))
+            .pipe(rename('docs/help.pdf'))
             .pipe(gulp.dest('./app/'));
+    });
+
+    gulp.task('clean', 'Remove useless directories.', function (cb) {
+        del([
+            'cache',
+            'dist'
+        ], cb);
+    });
+
+    gulp.task('build', 'Building application for distribution.', ['clean'], function () {
+        var nw = new NwBuilder({
+            files: [
+                './**/**',
+                '!cache/*',
+                '!dist/*',
+                '!node_modules/*',
+                '!pictures/*',
+                '!test/*',
+                '!.gitignore',
+                '!.jshintrc',
+                '!gulpfile.js',
+                '!README.md'
+            ],
+            platforms: ['win' /*, 'osx' /*, 'linux' */],
+            buildDir: './dist'
+        });
+        nw.on('log', console.log);
+        // Build returns a promise
+        nw.build().then(function () {
+            console.log('all done!');
+        }).catch(function (error) {
+            console.error(error);
+        });
     });
 
     // exports
