291af3a550b040bf09bbfa7a527c3f64ff0c4cf3
Piotr Kowalski <piecioshka@gmail.com>
2014-12-13 09:34:57 +0100

    Update help.pdf.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 (function () {
     'use strict';
 
+    // dependencies
     var gulp = require('gulp');
     var jshint = require('gulp-jshint');
     var stylish = require('jshint-stylish');
@@ -30,11 +31,11 @@
             .pipe(jshint.reporter(stylish, { verbose: true }));
     });
 
-    gulp.task('count', 'Count LOC each *.js file in app/scripts/core directory.', shell.task([
+    gulp.task('count', 'Count LOC of each *.js file in `app/scripts/core`.', shell.task([
         'find app/scripts/core -name "*.js" | xargs wc -l | sort -r'
     ]));
 
-    gulp.task('build-help', function () {
+    gulp.task('build-help', 'Building help.pdf from README.md.', function () {
         return gulp.src('README.md')
             .pipe(markdownpdf())
             .pipe(rename("docs/help.pdf"))
