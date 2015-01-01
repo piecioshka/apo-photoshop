3d6c551de49b40964990edb0b6f574df1d5667e4
Piotr Kowalski <piecioshka@gmail.com>
2015-01-01 15:57:30 +0100

    Fixing bulding package.

--- gulpfile.js
+++ gulpfile.js
@@ -27,54 +27,41 @@
         scripts: [path.join(__dirname, 'app', 'scripts', 'core', '**', '*js')]
     };
 
+    // -----------------------------------------------------------------------------------------------------------------
+
     gulp.task('lint', 'Validate each *.js file with JSHint.', function () {
         return gulp.src(paths.scripts)
             .pipe(jshint(jshintrc))
-            .pipe(jshint.reporter(stylish, {verbose: true}));
+            .pipe(jshint.reporter(stylish, { verbose: true }));
     });
 
     gulp.task('count', 'Count LOC of each *.js file in `app/scripts/core`.', shell.task([
         'find app/scripts/core -name "*.js" | xargs wc -l | sort -r'
     ]));
 
-    gulp.task('build-help', 'Building help.pdf from README.md.', function () {
+    gulp.task('build', 'Building application for distribution.', function () {
+        var nw = new NwBuilder({
+            files: [
+                'app/**/**',
+                'package.json'
+            ],
+            // win64, win32, osx64, osx32, linux
+            platforms: ['win64', 'osx64'],
+            buildDir: './dist'
+        });
+
+        nw.on('log', console.log);
+
+        nw.build().catch(function (error) {
+            console.error(error);
+        });
+    });
+
+    gulp.task('build:help', 'Building help.pdf from README.md.', function () {
         return gulp.src('README.md')
             .pipe(markdownpdf())
             .pipe(rename('docs/help.pdf'))
             .pipe(gulp.dest('./app/'));
-    });
-
-    gulp.task('clean', 'Remove useless directories.', function (cb) {
-        del([
-            'cache',
-            'dist'
-        ], cb);
-    });
-
-    gulp.task('build', 'Building application for distribution.', ['clean'], function () {
-        var nw = new NwBuilder({
-            files: [
-                './**/**',
-                '!cache/*',
-                '!dist/*',
-                '!node_modules/*',
-                '!pictures/*',
-                '!test/*',
-                '!.gitignore',
-                '!.jshintrc',
-                '!gulpfile.js',
-                '!README.md'
-            ],
-            platforms: ['win' /*, 'osx' /*, 'linux' */],
-            buildDir: './dist'
-        });
-        nw.on('log', console.log);
-        // Build returns a promise
-        nw.build().then(function () {
-            console.log('all done!');
-        }).catch(function (error) {
-            console.error(error);
-        });
     });
 
     // exports
